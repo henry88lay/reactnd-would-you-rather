89a9a901ff3c1ccb87b8ff847b409b9dc7598fa0
Henry Lay <henry88lay@gmail.com>
2019-09-22 19:24:34 +1000

    set up create-react-app

---
+++ src/components/App.js
@@ -0,0 +1,58 @@
+import React, {Component, Fragment} from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import {Grid} from 'semantic-ui-react';
+import {handleInitialData} from '../actions/shared';
+import {connect} from 'react-redux';
+import Login from './Login';
+import Nav from './Nav';
+import Home from './Home';
+
+class App extends Component {
+  componentDidMount() {
+    this.props.handleInitialData();
+  }
+  render() {
+    const {authUser} = this.props;
+    return (
+      <Router>
+        <div className='App'>
+          {authUser === null ? (
+            <Route
+              render={() => (
+                <ContentGrid>
+                  <Login />
+                </ContentGrid>
+              )}
+            />
+          ) : (
+            <Fragment>
+              <Nav />
+              <ContentGrid>
+                <Route exact path='/' component={Home} />
+              </ContentGrid>
+            </Fragment>
+          )}
+        </div>
+      </Router>
+    );
+  }
+}
+
+const ContentGrid = ({children}) => (
+  <Grid padded='vertically' columns={1} centered>
+    <Grid.Row>
+      <Grid.Column style={{maxWidth: 550}}>{children}</Grid.Column>
+    </Grid.Row>
+  </Grid>
+);
+
+function mapStateToProps({authUser}) {
+  return {
+    authUser
+  };
+}
+
+export default connect(
+  mapStateToProps,
+  {handleInitialData}
+)(App);
